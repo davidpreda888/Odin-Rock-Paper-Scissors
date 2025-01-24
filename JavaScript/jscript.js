e735dcd8f2220abb47e6eb69377a9707f715405c
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-01-24 15:45:10 -0500

    Complete playRound function

--- JavaScript/jscript.js
+++ JavaScript/jscript.js
@@ -12,13 +12,34 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let result = prompt('Choose between rock, paper or scissors');
+    result = result.toLowerCase();
     while (result!== 'rock' && result !== 'paper' && result !== 'scissors') {
         console.log('Invalid choice');
         result = prompt('Choose between rock, paper or scissors');
     }
-    return result.toLowerCase();
+    return result;
 }
 //console.log(getHumanChoice()); // rock, paper, or scissors
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Human score: ${humanScore}`);
+        return `It's a tie!`;
+    } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Human score: ${humanScore}`);
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Human score: ${humanScore}`);
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    }   
+}
