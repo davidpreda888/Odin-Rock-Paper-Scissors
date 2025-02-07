7a92e62e2af0047f905357347ee5b2ba062b7790
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-02-07 00:57:04 -0500

    Add styling and change to 5 rounds

--- JavaScript/jscript.js
+++ JavaScript/jscript.js
@@ -13,27 +13,32 @@ let humanScore = 0;
 let computerScore = 0;
 
 const score = document.querySelector(".score");
+const resultText = document.querySelector(".result");
 
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
-        score.textContent = `It's a tie! ${humanScore} - ${computerScore}`
+        resultText.textContent = `It's a tie!`
+        score.textContent = `${humanScore} - ${computerScore}`
         return;
 
     } else if (humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "scissors" && computerChoice === "paper") {
         humanScore++;
-        score.textContent = `You won this round: ${humanScore} - ${computerScore}`
+
+        resultText.textContent = `You won this round!`
+        score.textContent = `${humanScore} - ${computerScore}`
         return;
     } else {
         computerScore++;
-        score.textContent = `You lost this round: ${humanScore} - ${computerScore}`
+
+        resultText.textContent = `You lost this round!`
+        score.textContent = `${humanScore} - ${computerScore}`
         return;
     }
        
 }
-
 
 const options = document.querySelectorAll("button");
 
@@ -54,13 +59,13 @@ options.forEach(button => {
                 playRound("scissors",getComputerChoice());
                 break;
         }
-        if(humanScore === 3){
+        if(humanScore === 5){
             score.textContent = `You won ${humanScore} to ${computerScore}!`;
             humanScore = 0;
             computerScore = 0;
         }
 
-        if(computerScore === 3){
+        if(computerScore === 5){
             score.textContent = `You lost ${computerScore} to ${humanScore}`;
             humanScore = 0;
             computerScore = 0;
