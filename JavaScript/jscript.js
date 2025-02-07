dd9dda8017baa348d1193aafa9348e2a589d7c01
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-02-07 01:19:32 -0500

    Saving mods

--- JavaScript/jscript.js
+++ JavaScript/jscript.js
@@ -60,12 +60,14 @@ options.forEach(button => {
                 break;
         }
         if(humanScore === 5){
+            resultText.textContent = '';
             score.textContent = `You won ${humanScore} to ${computerScore}!`;
             humanScore = 0;
             computerScore = 0;
         }
 
         if(computerScore === 5){
+            resultText.textContent = '';
             score.textContent = `You lost ${computerScore} to ${humanScore}`;
             humanScore = 0;
             computerScore = 0;
