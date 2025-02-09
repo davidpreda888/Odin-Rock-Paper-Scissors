abd7467af2c02f4740623e86cc2f8c0589c53682
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-02-09 05:08:43 -0500

    Add Styling and description to front page

--- JavaScript/jscript.js
+++ JavaScript/jscript.js
@@ -60,15 +60,15 @@ options.forEach(button => {
                 break;
         }
         if(humanScore === 5){
-            resultText.textContent = '';
-            score.textContent = `You won ${humanScore} to ${computerScore}!`;
+            resultText.textContent = `You won ${humanScore} to ${computerScore}!`;
+            score.textContent = "Congratulations, you beat the Bot!";
             humanScore = 0;
             computerScore = 0;
         }
 
         if(computerScore === 5){
-            resultText.textContent = '';
-            score.textContent = `You lost ${computerScore} to ${humanScore}`;
+            resultText.textContent = `You lost ${computerScore} to ${humanScore}`;
+            score.textContent = "Better luck next time!";
             humanScore = 0;
             computerScore = 0;
         }
